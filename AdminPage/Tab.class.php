0ec823de22063e0cd679505f15721f3ba18b6aec
Andrei Surdu <smartik89@gmail.com>
2015-12-18 19:59:58 +0200

    Improvements to tabs
    Added enqueue possibility to tabs. Works exactly like pages and the
    scripts/styles are enqueued and accesible in all other tabs and parent
    page.

--- AdminPage/Tab.class.php
+++ AdminPage/Tab.class.php
@@ -11,6 +11,7 @@ namespace WPAP;
 
 class Tab{
 	
+	public $parent_page_id;
 	public $parent_page;
 	public $tab_id;
 
@@ -19,13 +20,60 @@ class Tab{
 			$this->parent_page = new Page( $parent_page_id );
 			$this->parent_page->add_tabs( array( $this, 'tab' ) ); 
 		}
+		$this->parent_page_id = $parent_page_id;
 		$this->tab_id = $tab_id;
+		add_action( 'admin_enqueue_scripts', array($this, 'adminEnqueue'), 30 );
 	}
 
+	//------------------------------------//--------------------------------------//
+	
+	/**
+	 * Admin enqueue
+	 *
+	 * Admin enqueue scripts and styles.
+	 * Designed to be extended in child class.
+	 *
+	 * @return void 
+	 */
+	public function enqueue(){}
+
+	//------------------------------------//--------------------------------------//
+	
+	/**
+	 * Admin enqueue only on this page
+	 *
+	 * Admin enqueue scripts and styles only on this page
+	 *
+	 * @return void 
+	 */
+	public function adminEnqueue(){
+		if( isset($_GET['page']) && $_GET['page'] == $this->parent_page_id ){
+			$this->enqueue();
+		}
+	}
+
+	//------------------------------------//--------------------------------------//
+	
+	/**
+	 * Tab Settings
+	 *
+	 * Define the tab settings.
+	 *
+	 * @return array 
+	 */
 	public function settings(){
 		return array();
 	}
 
+	//------------------------------------//--------------------------------------//
+	
+	/**
+	 * tab settings
+	 *
+	 * Define the tab settings. To not be used. Use 'settings()' instead
+	 *
+	 * @return array The final settings 
+	 */
 	public function getSettings(){
 		return wp_parse_args( 
 			array( 
@@ -36,10 +84,28 @@ class Tab{
 		);
 	}
 
+	//------------------------------------//--------------------------------------//
+	
+	/**
+	 * Tab content page
+	 *
+	 * This is what the tab should display
+	 *
+	 * @return array The final settings 
+	 */
 	public function page(){
 		_e('Congrats! you\'ve created a new page tab.', 'WPAP');
 	}
 
+	//------------------------------------//--------------------------------------//
+	
+	/**
+	 * Tab filter
+	 *
+	 * Add this tab to the list of tabs
+	 *
+	 * @return array All tabs 
+	 */
 	public function tab($tabs){
 		$tabs[] = $this->getSettings();
 		return $tabs;
