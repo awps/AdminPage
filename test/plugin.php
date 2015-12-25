251e20a36c2bda03244850023d0d49ae485519f1
Andrei Surdu <smartik89@gmail.com>
2015-12-25 18:46:24 +0200

    Added test files
    Needs to include 'src' folder

---
+++ test/plugin.php
@@ -0,0 +1,38 @@
+<?php
+/* 
+ * Plugin Name: ZeroWp AdminPage Test
+ */
+
+require_once dirname(__FILE__) . "/src/AdminPage/mod.php";
+
+
+class ExamplePage extends ZeroWP\Admin\Page{
+
+    public function settings(){
+        return array(
+            'menu_type'     => 'menu',
+            'menu_title'    => __('Example page', 'text-domain'),
+        );
+    }
+
+    public function page(){
+      echo 'Page content here!';
+    }
+}
+$example_page = new ExamplePage('page-slug');
+$example_page->init(); //Initialize this page.
+
+
+class ExampleTab extends ZeroWP\Admin\Tab{
+
+    public function settings(){
+        return array(
+            'label' => __('Tab title', 'text-domain')
+        );
+    }
+
+    public function page(){
+      echo 'Tab content here!';
+    }
+}
+new ExampleTab('tabid', 'page-slug');
