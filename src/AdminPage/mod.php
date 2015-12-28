d379c6daeda39da8e951f64f3f43a57058f042c3
Andrei Surdu <smartik89@gmail.com>
2015-12-28 14:32:42 +0200

    Added mod info & License

--- src/AdminPage/mod.php
+++ src/AdminPage/mod.php
@@ -1,3 +1,14 @@
 <?php
+/* 
+ * Mod Name:    ZeroWP AdminPage
+ * Mod URI:     http://zerowp.com/
+ * Description: Advanced WordPress Admin pages, the easy way...
+ * Author:      ZeroWP Team
+ * Version:     1.0
+ * Author URI:  http://zerowp.com/
+ * Licence:     GPLv2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Copyright:   (c) 2015 ZeroWP. All rights reserved
+ */
 require_once dirname(__FILE__) . "/Page.class.php";
 require_once dirname(__FILE__) . "/Tab.class.php";
